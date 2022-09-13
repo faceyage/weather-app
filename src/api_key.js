2d3e715421a3b40be9f5b1f1123ebea36ffc8621
faceage <alperencingoz33@gmail.com>
2022-09-13 16:40:13 +0300

    Add API_KEY

--- src/api_key.js
+++ src/api_key.js
@@ -1,3 +1,4 @@
 //Share this file because openweathermap is free
+//it's also not for webpack since it's creates another js file
 //but for other api's don't upload this to github
 export const api_key = "b190a0605344cc4f3af08d0dd473dd25";
